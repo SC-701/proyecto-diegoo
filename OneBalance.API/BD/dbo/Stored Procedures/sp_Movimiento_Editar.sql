057008fd40e2564eff5d2175ee9f856dcdbdcacb
diego <diegoalonsonm@gmail.com>
2025-08-12 18:24:38 -0600

    ajsute en la tabla de movimiento

--- OneBalance.API/BD/dbo/Stored Procedures/sp_Movimiento_Editar.sql
+++ OneBalance.API/BD/dbo/Stored Procedures/sp_Movimiento_Editar.sql
@@ -1,10 +1,10 @@
 ﻿
 -- EDITAR
-CREATE PROCEDURE sp_Movimiento_Editar (@idMovimiento UNIQUEIDENTIFIER, @idCategoria UNIQUEIDENTIFIER, @idTipoPago UNIQUEIDENTIFIER, @descripcion NVARCHAR(MAX), @montoOriginal DECIMAL(18,2),
-    @monedaOriginal NVARCHAR(50), @montoCrc DECIMAL(18,2), @tasaCambio DECIMAL(18,6), @estado BIT)
+CREATE PROCEDURE sp_Movimiento_Editar (@idMovimiento UNIQUEIDENTIFIER, @idCategoria UNIQUEIDENTIFIER, @idTipoPago UNIQUEIDENTIFIER, @descripcion NVARCHAR(MAX), 
+    @montoOriginal DECIMAL(18,2), @estado BIT)
 AS
 BEGIN
-    UPDATE Movimiento SET idCategoria = @idCategoria, idTipoPago = @idTipoPago, descripcion = @descripcion, montoOriginal = @montoOriginal, monedaOriginal = @monedaOriginal, montoCrc = @montoCrc,
-        tasaCambio = @tasaCambio, estado = @estado WHERE idMovimiento = @idMovimiento;
+    UPDATE Movimiento SET idCategoria = @idCategoria, idTipoPago = @idTipoPago, descripcion = @descripcion, montoOriginal = @montoOriginal, 
+        estado = @estado WHERE idMovimiento = @idMovimiento;
 	select @idMovimiento;
 END;
